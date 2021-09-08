da88e9c81eba02265dd9979b037aec439c82c107
Frans Jansen <info@fransjansen.eu>
2021-09-08 12:10:12 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+console.log("Hallo Winc Academy studenten")
+//Dit is een grote som (Deze comment heeft geen invloed op de uitkomst van de some omdat het een regel-comment is die boven de regel met de som staat)
+/* console.log(1230941 * 1823794)
+console.log(637263 / 54) */
